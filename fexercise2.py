e39c10f9902096921f8010a857813bb94fe98f6c
lsmagno1 <60122405+lsmagno1@users.noreply.github.com>
2020-03-08 19:11:57 +0800

    Adding functions exercise 2

---
+++ fexercise2.py
@@ -0,0 +1,22 @@
+errormessage = "Bad score"
+score = input('Enter a score between 0.0 and 1.0:')
+
+def computegrade (score):
+ try:
+    score = float(score)
+    if score <0.0 or score >= 1.0:
+        print (errormessage)
+    elif score >= 0.9:
+        print ('A')
+    elif score >= 0.8:
+        print ('B')
+    elif score >= 0.7:
+        print ('C')
+    elif score >= 0.6:
+        print ('D')
+    elif score < 0.6:
+        print ('F')
+ except:
+    print(errormessage)
+
+computegrade (score)
